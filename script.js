e476fcd07495962a22acadd45bd3998b80d33cfc
myingineer <e.alexander190@yahoo.com>
2023-08-14 17:23:04 +0100

    edited-version

--- script.js
+++ script.js
@@ -22,124 +22,68 @@ const errorDisplay2 = document.querySelector('.js-third-input');
 const resetButton = document.querySelector('.js-reset-btn');
 
 
+// To display the error
+function showErrorMessage() {
+    tipAmountDisplay.innerHTML = 0;
+    totalAmountDisplay.innerHTML = 0;
+    errorDisplay.style.display = 'block';
+    errorDisplay2.classList.add('error2');
+}
+
 // To remove the error display.
 function hideErrorMessage() {
     errorDisplay.style.display = 'none';
     errorDisplay2.classList.remove('error2');
 }
 
-// Fixed calculation for 5% tip btn.
-fivePercentTipBtn.addEventListener('click', () => {
+
+function calculateTip(percentValue) {
     let billAmount = amountInput.value;
     if (!isNaN(billAmount) && !isNaN(numberOfPersonInput.value)) {
-        let five_percent = ((((+billAmount * 100) * (5 / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
-        tipAmountDisplay.innerHTML = five_percent;
+        let percentTip = ((((+billAmount * 100) * (percentValue / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
+        tipAmountDisplay.innerHTML = percentTip;
+
 
         if (!isNaN(numberOfPersonInput.value) && numberOfPersonInput.value !== '') {
-            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+five_percent * 100)) / 100).toFixed(2);
+            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+percentTip * 100)) / 100).toFixed(2);
         } else {
-            tipAmountDisplay.innerHTML = 0;
-            totalAmountDisplay.innerHTML = 0;
-            errorDisplay.style.display = 'block';
-            errorDisplay2.classList.add('error2');
+            showErrorMessage();
             setTimeout(() => {
                 hideErrorMessage();
             }, 2000);
-        }
-        
-    }
+        };
+    };
+};
+
+
+
+// Fixed calculation for 5% tip btn.
+fivePercentTipBtn.addEventListener('click', () => {
+    calculateTip(5);
 })
 
 
 // Fixed calculation for 10% tip btn.
 tenPercentTipBtn.addEventListener('click', () => {
-    let billAmount = amountInput.value;
-    if (!isNaN(billAmount) && !isNaN(numberOfPersonInput.value)) {
-        let ten_percent = ((((+billAmount * 100) * (10 / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
-        tipAmountDisplay.innerHTML = ten_percent;
-
-        if (!isNaN(numberOfPersonInput.value) && numberOfPersonInput.value !== '') {
-            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+ten_percent * 100)) / 100).toFixed(2);
-        } else {
-            tipAmountDisplay.innerHTML = 0;
-            totalAmountDisplay.innerHTML = 0;
-            errorDisplay.style.display = 'block';
-            errorDisplay2.classList.add('error2');
-            setTimeout(() => {
-                hideErrorMessage();
-            }, 2000);
-        }
-        
-    }
+    calculateTip(10);   
 })
 
 
 // Fixed calculation for 15% tip btn.
 fifteenPercentTipBtn.addEventListener('click', () => {
-    let billAmount = amountInput.value;
-    if (!isNaN(billAmount) && !isNaN(numberOfPersonInput.value)) {
-        let fifteen_percent = ((((+billAmount * 100) * (15 / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
-        tipAmountDisplay.innerHTML = fifteen_percent;
-
-        if (!isNaN(numberOfPersonInput.value) && numberOfPersonInput.value !== '') {
-            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+fifteen_percent * 100)) / 100).toFixed(2);
-        } else {
-            tipAmountDisplay.innerHTML = 0;
-            totalAmountDisplay.innerHTML = 0;
-            errorDisplay.style.display = 'block';
-            errorDisplay2.classList.add('error2');
-            setTimeout(() => {
-                hideErrorMessage();
-            }, 2000);
-        }
-        
-    }
+    calculateTip(15);
 })
 
 
 // Fixed calculation for the 25% tip btn.
 twentyFivePercentTipBtn.addEventListener('click', () => {
-    let billAmount = amountInput.value;
-    if (!isNaN(billAmount) && !isNaN(numberOfPersonInput.value)) {
-        let twentyFive_percent = ((((+billAmount * 100) * (25 / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
-        tipAmountDisplay.innerHTML = twentyFive_percent;
-
-        if (!isNaN(numberOfPersonInput.value) && numberOfPersonInput.value !== '') {
-            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+twentyFive_percent * 100)) / 100).toFixed(2);
-        } else {
-            tipAmountDisplay.innerHTML = 0;
-            totalAmountDisplay.innerHTML = 0;
-            errorDisplay.style.display = 'block';
-            errorDisplay2.classList.add('error2');
-            setTimeout(() => {
-                hideErrorMessage();
-            }, 2000);
-        }
-        
-    }
+    calculateTip(25);
 })
 
 
 // Fixed calculation for the 50% tip btn.
 fiftyPercentTipBtn.addEventListener('click', () => {
-    let billAmount = amountInput.value;
-    if (!isNaN(billAmount) && !isNaN(numberOfPersonInput.value)) {
-        let fifty_percent = ((((+billAmount * 100) * (50 / 100)) / 100) / +numberOfPersonInput.value).toFixed(2);
-        tipAmountDisplay.innerHTML = fifty_percent;
-
-        if (!isNaN(numberOfPersonInput.value) && numberOfPersonInput.value !== '') {
-            totalAmountDisplay.innerHTML = ((((+billAmount * 100) / +numberOfPersonInput.value) + (+fifty_percent * 100)) / 100).toFixed(2);
-        } else {
-            tipAmountDisplay.innerHTML = 0;
-            totalAmountDisplay.innerHTML = 0;
-            errorDisplay.style.display = 'block';
-            errorDisplay2.classList.add('error2');
-            setTimeout(() => {
-                hideErrorMessage();
-            }, 2000);
-        }
-        
-    }
+    calculateTip(50);
 })
 
 // Calculation for custom tip input.
